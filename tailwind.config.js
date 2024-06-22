cb4435f83e43647267dd973e0dcffdfa01f3470d
Bat <banthony@student.ciputra.ac.id>
2024-06-22 23:38:24 +0700

    First Setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./CSS/*.css",
+    "./HTML/*.html",
+    "./JS/*.js",
+    "./tailwind.css"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
